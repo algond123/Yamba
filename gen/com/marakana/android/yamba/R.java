1f7344164e441b5f3ab267710fb32ebc289bc697
Duver Grajales <duvernal3@gmail.com>
2014-08-30 11:23:10 -0500

    Version2

--- gen/com/marakana/android/yamba/R.java
+++ gen/com/marakana/android/yamba/R.java
@@ -30,7 +30,7 @@ public final class R {
         public static final int status_text_count=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_status=0x7f030000;
+        public static final int fragment_status=0x7f030000;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -39,14 +39,11 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
-        public static final int password=0x7f050008;
-        public static final int password_summary=0x7f050009;
         public static final int status_button_tweet=0x7f050003;
         public static final int status_text_hint=0x7f050004;
-        public static final int status_update=0x7f05000a;
-        public static final int tweet=0x7f050005;
-        public static final int username=0x7f050006;
-        public static final int username_summary=0x7f050007;
+        public static final int status_update=0x7f050007;
+        public static final int title_activity_main=0x7f050005;
+        public static final int tweet=0x7f050006;
     }
     public static final class style {
         /** 
