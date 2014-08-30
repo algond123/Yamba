3d3ab4f5f2f884067c3b2d977346f65dd269a154
Duver Grajales <duvernal3@gmail.com>
2014-08-30 11:30:52 -0500

    Version8

--- gen/com/marakana/android/yamba/R.java
+++ gen/com/marakana/android/yamba/R.java
@@ -47,6 +47,7 @@ public final class R {
         public static final int activity_main=0x7f030000;
         public static final int fragment_status=0x7f030001;
         public static final int list_item=0x7f030002;
+        public static final int widget=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f090000;
