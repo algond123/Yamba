30d1bf6eea3b09a05b044d7b493ff43b8b02c4f5
Duver Grajales <duvernal3@gmail.com>
2014-08-30 11:28:21 -0500

    Version6

--- gen/com/marakana/android/yamba/R.java
+++ gen/com/marakana/android/yamba/R.java
@@ -28,17 +28,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_purge=0x7f0a0006;
-        public static final int action_refresh=0x7f0a0005;
-        public static final int action_settings=0x7f0a0003;
-        public static final int action_tweet=0x7f0a0004;
-        public static final int status_button_tweet=0x7f0a0000;
-        public static final int status_text=0x7f0a0001;
-        public static final int status_text_count=0x7f0a0002;
+        public static final int action_purge=0x7f0a000d;
+        public static final int action_refresh=0x7f0a000c;
+        public static final int action_settings=0x7f0a000a;
+        public static final int action_tweet=0x7f0a000b;
+        public static final int fragment_details=0x7f0a0001;
+        public static final int fragment_timeline=0x7f0a0000;
+        public static final int list_item_content=0x7f0a0005;
+        public static final int list_item_freshness=0x7f0a0009;
+        public static final int list_item_text_created_at=0x7f0a0007;
+        public static final int list_item_text_message=0x7f0a0008;
+        public static final int list_item_text_user=0x7f0a0006;
+        public static final int status_button_tweet=0x7f0a0002;
+        public static final int status_text=0x7f0a0003;
+        public static final int status_text_count=0x7f0a0004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int fragment_status=0x7f030001;
+        public static final int list_item=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f090000;
